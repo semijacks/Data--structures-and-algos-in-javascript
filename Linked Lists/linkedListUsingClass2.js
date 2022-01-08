9c7aa359e07a462c005b36f6897f8c35336deb88
Semilore Idowu <semijacks27@gmail.com>
2022-01-08 12:36:47 +0100

    add some more linked lists methods

--- Linked Lists/linkedListUsingClass2.js
+++ Linked Lists/linkedListUsingClass2.js
@@ -11,6 +11,16 @@ class LinkedList {
     this.length = 1;
   }
 
+  printList() {
+    let array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
   append(value) {
     const newNode = new Node(value);
     this.tail.next = newNode;
@@ -26,10 +36,48 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  insert(index, value) {
+    // check params
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = new Node(value);
+    const leaderNode = this.traverseToIndex(index - 1);
+    const holdingPointer = leaderNode.next;
+    leaderNode.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this;
+  }
+
+  traverseToIndex(index) {
+    // check params
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  remove(index) {
+    // check params
+    const leaderNode = this.traverseToIndex(index - 1);
+    const indexNode = leaderNode.next;
+    leaderNode.next = indexNode.next;
+    this.length--;
+    return this;
+  }
 }
 
 const myLinkedList = new LinkedList(15);
 myLinkedList.append(27);
 myLinkedList.append(50);
 myLinkedList.prepend(10);
-console.log(myLinkedList);
+myLinkedList.insert(2, 45);
+myLinkedList.insert(20, 88);
+myLinkedList.insert(4, 97);
+myLinkedList.remove(4);
+console.log(myLinkedList.printList());
